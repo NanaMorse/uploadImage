bddafd63f5d8aab8ef1b3e08cdd534b1bc7174dc
Morse <nana.morse@outlook.com>
2015-12-25 17:47:16 +0800

    获取并缓存jquery对象

---
+++ public/js/jqueryMap.js
@@ -0,0 +1,10 @@
+// 用以缓存jQuery对象
+define(function (require, exports, module) {
+    var $ = require("jQuery");
+
+
+    module.exports = {
+        // 上传文件的输入按钮
+        "uploadFileBtn" : $("#uploadFile")
+    };
+});
