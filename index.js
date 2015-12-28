1e0db9e75a295d8f619d6dcd40f7a53ee12f34f7
Morse <nana.morse@outlook.com>
2015-12-28 17:13:54 +0800

    引入body-parser中间件，处理base64存储至本地

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 "use strict";
 var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
 var app = express();
 
 // 设置固定端口号
@@ -11,10 +13,23 @@ app.set("PORT", 8888);
 
 // 设置静态资源目录
 app.use(express.static(__dirname + '/public'));
+// 设置body中间件，用于读取post数据
+app.use(bodyParser.json({"limit" : "50mb"}));
+app.use(bodyParser.urlencoded({"extended" : true, "limit" : "50mb"}));
 
 // 设置路由
 app.get("/", function(req, res){
     res.redirect("/index.html");
+});
+
+// 处理图片上传请求
+app.post("/uploadImage", function(req, res){
+    var i, imgInfo,
+        receiveData = req.body.imageData;
+    for(i = 0; (imgInfo = receiveData[i]) != null; i++){
+        savingImageData("./receives/images/", imgInfo.dataUrl, imgInfo.fileName);
+    }
+    res.send("upload success!");
 });
 
 // 404
@@ -36,4 +51,15 @@ app.use(function(err, req, res, next){
 app.listen(app.get("PORT"), function(){
     console.log("web server started at " + app.get("PORT"));
 });
+
+// 写入数据到本地
+function savingImageData(dst, data, fileName){
+    //todo 如何将base64转换为readStream数据？
+    fs.writeFile((dst ? dst : "") + fileName, data, "base64", function(err){
+        if(err) throw err;
+        else {
+            console.log(fileName + "已存储至" + (dst ? dst : "当前目录") + "下。");
+        }
+    });
+}
 
