b1630866b89118140c98e67986362a330281b81a
Morse <nana.morse@outlook.com>
2015-12-28 17:15:53 +0800

    生成上传图片的数据集合

--- public/js/shell.js
+++ public/js/shell.js
@@ -2,8 +2,10 @@
  * Created by nana on 2015/12/25.
  */
 define(function(require, exports, module){
+    "use strict";
     // 获取缓存的jquery对象
-    var jqueryMap = require("./jqueryMap");
+    var jqueryMap = require("./jqueryMap"),
+        uploader = require("./uploader");
 
     function init(){
         bindPageEvent();
@@ -13,25 +15,49 @@ define(function(require, exports, module){
     function bindPageEvent(){
         // input内容改变事件
         jqueryMap.uploadFileBtn.bind("change", function(e){
-            var i, file, fileReader,
+            var i, file, fileReader, fileName,
                 results = [],
                 files = e.target.files,
                 count = files.length;
             for(i = 0; (file = files[i]) != null; i++){
                 fileReader = new FileReader();
                 fileReader.readAsDataURL(file);
-                (function(i){
+                fileName = file.name;
+                console.log(file);
+                (function(i, fileName){
                     fileReader.onload = function(e){
-                        results[i] = e.target.result;
+                        results[i] = {
+                            "fileName" : fileName,
+                            "dataUrl" : e.target.result.split(",")[1]
+                        };
                         count--;
                         if(count === 0){
-                            // 在此处将结果传递给modal模块，用于显示modal框，确认图片上传
-                            console.log(results);
+                            // 全部上传完毕后重置input
+                            resetInputFile(jqueryMap.uploadFileBtn[0]);
+                            //todo 在此处将结果传递给modal模块，用于显示modal框，确认图片上传
+                            // 直接启动上传
+                            uploader.postImageData({
+                                "imageData" : results
+                            });
                         }
                     }
-                })(i);
+                })(i, fileName);
             }
         });
+    }
+
+    // 重置input
+    function resetInputFile(file){
+        file.value = "";
+        // 在IE11以下中无法清除input内容
+        if(file.value){
+            var fakeFrom = document.createElement("form"),
+                realForm = file.parentNode,
+                afterFile = file.nextSibling;
+            fakeFrom.appendChild(file);
+            fakeFrom.reset();
+            realForm.insertBefore(file, afterFile);
+        }
     }
     
 
--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,5 +1,15 @@
 define(function (require, exports, module) {
+    "use strict";
+    var $ = require("jQuery");
 
+    // 获取数组格式的base64数据集合，进行ajax上传
+    function postImageData(dataArr){
+        $.post("/uploadImage", dataArr, function(data){
+            console.log(data);
+        });
+    }
 
-    module.exports = {};
+    module.exports = {
+        "postImageData" : postImageData
+    };
 });
